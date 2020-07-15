11bb80b3b7c5bbe068645b9e66071de604aa51e8
Maksim Kolasau <maxbailey182@gmail.com>
2020-07-15 15:40:28 +0300

    Lesson4-5

--- Lesson_4_Variables.py
+++ Lesson_4_Variables.py
@@ -7,4 +7,12 @@ num_2 = int(input("Number 2 : "))
 result = num_1 + num_2
 print("Result is : " + str(result))
 # del result
- 
+
+
+
+
+
+
+
+
+
---
+++ Lesson_5_if-elif-else_.py
@@ -0,0 +1,20 @@
+
+num = input("Enter number: ")
+
+if int(num) > 10:
+    print("More than 10\n")
+elif int(num) < 0:
+    print("Less than 0")
+else:
+    print("You are in range of 10")
+
+
+name = input()
+A = "YES" if name != "Test" else "No"
+print(A)
+
+
+
+
+
+
