ee6bb59c52163a82e75e1f7e4e92d961e89089fb
Maksim Kolasau <maxbailey182@gmail.com>
2020-07-15 15:02:56 +0300

    Lesson_4_Dudar

---
+++ Lesson_4_Variables.py
@@ -0,0 +1,10 @@
+
+# a = int(input())
+# print(a ** 2)
+
+num_1 = int(input("Number 1 : "))
+num_2 = int(input("Number 2 : "))
+result = num_1 + num_2
+print("Result is : " + str(result))
+# del result
+ 
